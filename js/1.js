b3422963b3ef65426b9b650b5728c5e1549a0c5c
no jae gun <njg7194@naver.com>
2021-04-20 19:16:25 +0900

    test

--- js/1.js
+++ js/1.js
@@ -1,5 +1,5 @@
 document.getElementById("demo").innerHTML = "Hello JavaScript!";
 //alert('hellow  ');
-function reset(){setTimeout('location.reload()',2000)};
+function reset(){setTimeout('location.reload()',200)};
 reset();
 
