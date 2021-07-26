8e9c135138e08403ffc1d8739dcd8898a84733dc
njg7194 <njg7194@naver.com>
2021-07-26 17:30:38 +0900

    공익 퇴근 커밋 / 코딩애플 map함수 실습 중.

--- ex-react/src/App.js
+++ ex-react/src/App.js
@@ -6,45 +6,31 @@ import './App.css';
 function App() {
 
   let[modal, modalSet] = useState(false);
-
-  return (
-    <div className="navbar-fix">
-
-      <div className="main-nav" onClick={ ()=>{modalSet(!modal)} }>
-        <div>uStock</div>
-      </div>
-
-      <List></List>
-      <List></List>
-      <List></List>
-
-      {
-        modal === true
-        ? <Modal></Modal>
-        : null
-      }
-
-      <video>df</video>
-    </div>
-  );
-
-
-}
-
-function List() {
-
   let [man,manSet] = useState(['남자 코트 추천', '우동', '독학']);
   let [tb, tbSet] = useState(0);
 
-  
-  return(
-    <div className="list">
-      <h3> {man[0]} <span className="tabon" onClick={() => { tbSet(tb + 1) }}>❤</span>{tb}</h3>
-      <p>7월 24일 발행함</p>
-      <hr />
-      <button className="changeVal" onClick={stateChange}>값 바꾸기</button>
+  return (
+    <div>
+      <div className="main-nav" onClick={() => { modalSet(!modal) }}> uStock </div>
+
+      {
+        man.map((글)=>{
+            <div className="list">
+              <h3> {글} <span className="tabon" onClick={() => { tbSet(tb + 1) }}>❤</span>{tb}</h3>
+              <p>7월 24일 발행함</p>
+              <hr />
+              <button className="changeVal" onClick={stateChange}>값 바꾸기</button>
+            </div>
+        })
+      }
+      {
+        modal === true
+          ? <Modal></Modal>
+          : null
+      }
     </div>
-  )
+  );
+
 
   function stateChange() {
     if(man[0].includes('여자'))
@@ -55,9 +41,11 @@ function List() {
       ex = ['여자 코트 추천', '라면', '학원']
       manSet(ex);
       console.log(man);
+      
     }
     
   }
+
 }
 
 function Modal() {
