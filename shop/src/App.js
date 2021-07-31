0eac71653ed7a90c7d22eb0253764d7840cde0a5
no jae gun <njg7194@naver.com>
2021-07-31 23:43:40 +0900

    집에서 커밋 / 코딩애플 / Component화+반복문 완료

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,16 @@
+/* eslint-disable */
+
 import './App.css';
 import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem, Container} from 'react-bootstrap';
+import importdata from './data.js'
+import { useState, props } from 'react';
 
 
 function App() {
+
+  let[data,dataSet] = useState(importdata);
+
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
@@ -30,31 +38,40 @@ function App() {
         <a>sfjlsa;g;asdjfs;ljf</a>
       </div>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-        </div>
-      </div>
 
-
+      <Shoelist data = {data} ></Shoelist>
 
 
     </div>
   );
 }
+
+function Shoelist(props) {
+
+  let tmp = [...props.data];
+
+  return (
+    <>
+
+      <div className="container">
+        <div className="row">
+          {
+            tmp.map((x, i) => {
+              return (
+                <div key={i} className="col-md-4">
+                  <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                  <h4>{x.title}</h4>
+                  <p>{x.content}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+
+    </>
+  )
+}
+
 
 export default App;
